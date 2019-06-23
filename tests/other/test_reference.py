9c2b3df1791e505b0c8dcec353a9a7fedbe39c39
Mohammed H Alshehri <542855+mhalshehri@users.noreply.github.com>
2019-06-23 22:12:38 +0300

    update reference tests

--- tests/other/test_reference.py
+++ tests/other/test_reference.py
@@ -1,13 +1,13 @@
-from datetime import date
 import pytest
 import pathlib
 import json
 import epiweeks
+from datetime import date
 
 
 def load_params_from_json(file):
-    jsonfile = pathlib.Path(__file__).parent.joinpath("fixtures", file)
-    content = jsonfile.read_text(encoding="utf-8")
+    json_file = pathlib.Path(__file__).parent.joinpath("data", file)
+    content = json_file.read_text(encoding="utf-8")
     data = [[tuple(i) for i in x] for x in json.loads(content)]
     params = [tuple(x) for x in data]
     params_reversed = [tuple(x[::-1]) for x in data]
@@ -21,24 +21,24 @@ iso_weeks = load_params_from_json("iso_weeks.json")
 @pytest.mark.parametrize("test_input, expected", cdc_weeks[0])
 def test_cdc_week_to_startdate(test_input, expected):
     year, week = test_input
-    startdate = epiweeks.Week(year, week, "cdc").startdate()
+    startdate = epiweeks.Week(year, week, "CDC").startdate()
     assert startdate.timetuple()[:3] == expected
 
 
 @pytest.mark.parametrize("test_input, expected", cdc_weeks[1])
 def test_cdc_week_from_startdate(test_input, expected):
-    week = epiweeks.Week.fromdate(date(*test_input), "cdc").weektuple()
+    week = epiweeks.Week.fromdate(date(*test_input), "CDC").weektuple()
     assert week == expected
 
 
 @pytest.mark.parametrize("test_input, expected", iso_weeks[0])
 def test_iso_week_to_startdate(test_input, expected):
     year, week = test_input
-    startdate = epiweeks.Week(year, week, "iso").startdate()
+    startdate = epiweeks.Week(year, week, "ISO").startdate()
     assert startdate.timetuple()[:3] == expected
 
 
 @pytest.mark.parametrize("test_input, expected", iso_weeks[1])
 def test_iso_week_from_startdate(test_input, expected):
-    week = epiweeks.Week.fromdate(date(*test_input), "iso").weektuple()
+    week = epiweeks.Week.fromdate(date(*test_input), "ISO").weektuple()
     assert week == expected
