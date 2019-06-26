416a91ab0e2dfa3e00b736e3c8e9279f9d5d5977
Mohammed H Alshehri <542855+mhalshehri@users.noreply.github.com>
2019-06-26 15:59:20 +0300

    bump version to 2.1.0

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ changelog = (here / "CHANGELOG.rst").read_text(encoding="utf-8")
 
 setup(
     name="epiweeks",
-    version="2.0.0",
-    description="Epidemiological weeks based on the CDC (MMWR) and ISO (WHO) "
-    "calculation methods.",
+    version="2.1.0",
+    description="Epidemiological weeks based on the CDC (MMWR) and ISO week "
+    "numbering systems.",
     long_description="\n".join([readme, changelog]),
     long_description_content_type="text/x-rst",
     url="https://github.com/dralshehri/epiweeks",
@@ -23,6 +23,7 @@ setup(
         "Intended Audience :: Healthcare Industry",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
@@ -30,7 +31,7 @@ setup(
         "Topic :: Scientific/Engineering",
         "Topic :: Utilities",
     ],
-    keywords="epidemiology weeks cdc mmwr iso who calendar surveillance "
+    keywords="epidemiology weeks cdc mmwr iso calendar surveillance "
     "public-health",
     py_modules=[module.stem for module in here.glob("src/*.py")],
     package_dir={"": "src"},
