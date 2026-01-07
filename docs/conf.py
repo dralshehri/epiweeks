8522198dfa0ac5e9ac97977d279a71c7a42f0bc5
Mohammed Alshehri <542855+dralshehri@users.noreply.github.com>
2026-01-07 18:51:23 +0300

    Update documentation configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,34 +1,51 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# Full list of options can be found in the Sphinx documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""Configuration file for the Sphinx documentation builder.
 
-import re
+The full list of options can be found in the Sphinx documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+
 import sys
+
 from pathlib import Path
 
-# Set package name and path
-package_name = "epiweeks"
+import tomllib
+
+
+# Load project configuration from pyproject.toml
+def load_project_config():
+    """Load project configuration from pyproject.toml."""
+    pyproject_path = Path("../pyproject.toml")
+    with open(pyproject_path, "rb") as f:
+        data = tomllib.load(f)
+    return data["project"]
+
+
+# Get project configuration
+project_config = load_project_config()
+
+# Set the package name and path
+package_name = project_config["name"]
 package_path = Path("../src").joinpath(package_name).resolve()
 sys.path.append(str(package_path.parent))
 
 
-# Extract package version
-def read_version():
-    content = package_path.joinpath("__init__.py").read_text()
-    pattern = re.compile(r"(?<=__version__\s=\s\").*(?=\")")
-    return pattern.search(content).group()
+def get_version():
+    """Return the current version of the package."""
+    with open("../pyproject.toml", "rb") as f:
+        data = tomllib.load(f)
+    return data["project"]["version"]
 
 
-# Add custom extensions to path
-sys.path.append(str(Path("_extensions").resolve()))
+# Add custom extensions to the system path
+sys.path.append(str(Path("_ext").resolve()))
 
+#
 # -- Project information ---------------------------------------------------------------
-
-project = "EpiWeeks"  # project name at PyPI and GitHub
-author = "Mohammed Alshehri"
-project_copyright = "2018 Mohammed Alshehri"
-version = read_version()
+#
+project = "EpiWeeks"  # Project name at PyPI and GitHub
+author = project_config["authors"][0]["name"]
+project_copyright = author
+version = project_config["version"]
 
 #
 # -- General configuration -------------------------------------------------------------
@@ -41,6 +58,7 @@ extensions = [
     "myst_parser",
     "notfound.extension",
     "custom_sitemap",
+    "custom_manpage",
 ]
 
 exclude_patterns = ["manpage.*"]
@@ -62,7 +80,7 @@ autodoc_default_options = {
     "undoc-members": False,
     "show-inheritance": True,
 }
-autoclass_content = "both"
+autoclass_content = "class"
 autodoc_typehints = "description"
 
 #
@@ -106,16 +124,12 @@ html_baseurl = f"https://{package_name}.readthedocs.io/en/stable/"
 html_title = f"{project} Documentation"
 html_theme = "furo"
 html_theme_options = {
-    "top_of_page_button": None,
+    "top_of_page_buttons": [],
     "footer_icons": [
         {
             "name": "GitHub",
-            "url": "https://github.com/dralshehri/hijridate",
-            "html": """
-                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
-                </svg>
-            """,
+            "url": project_config["urls"]["Repository"],
+            "html": '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>',  # noqa: E501
             "class": "",
         },
     ],
@@ -142,6 +156,4 @@ html_show_sphinx = False
 #
 # -- Options for manual pages output ---------------------------------------------------
 #
-man_pages = [
-    ("manpage", package_name, "calculate epidemiological weeks", author, 7)
-]
+man_pages = [("manpage", package_name, "calculate epidemiological weeks", author, 7)]
